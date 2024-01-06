58d893963af6a86ebd435eefedcc211c85e838e8
Alok Debnath <alokdebnath.in@gmail.com>
2024-01-06 10:19:09 +0000

    nothing majour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
+"use client"
 import { NavbarSimple } from '@/components/Navbar';
+// import styled from 'styled-components';
+
+// const P_green = styled.p`
+//   color: rgb(100, 255, 218);
+// `;
+// const P_regular = styled.p`
+//   color: rgb(136, 146, 176);
+// `;
+
+// const Div_main = styled.div`
+//   background-color: rgb(10, 25, 47);
+// `;
+// const H1_focused = styled.h1`
+//   color: rgb(204, 214, 246);
+// `;
 
 const Home = () => {
   return (
     <>
-      <div className='mx-auto min-w-screen min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500 p-4'>
+      <div className='mx-auto min-w-screen min-h-screen p-4'>
         <div className='mx-auto max-w-screen-2xl'>
           <NavbarSimple />
           <div className='mx-3 mt-6'>
-            <h1 className='text-black'>Hello World!</h1>
-            <h1 className='text-black mt-96 mb-96'>Hello World!</h1>
+            <div className='gap-y-16'>
+              <p>Hi, my name is</p>
+              <h1 className='font-bold text-6xl'>Alok Debnath.</h1>
+            </div>
           </div>
         </div>
       </div>
